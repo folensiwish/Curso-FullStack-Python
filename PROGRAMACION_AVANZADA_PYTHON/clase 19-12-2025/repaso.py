da3bc75807f43d8eb573c8b154d9b8608a87bfbb
folensiwish <fer.albertoandres@gmail.com>
2025-12-30 01:51:08 -0300

    ejercicios_repaso_POO

--- PROGRAMACION_AVANZADA_PYTHON/clase 19-12-2025/repaso.py
+++ PROGRAMACION_AVANZADA_PYTHON/clase 19-12-2025/repaso.py
@@ -42,7 +42,8 @@ REGLAS:
 - Usar un método estático para validar el año
 """
 
-mensaje = """
+#mensaje = 
+"""
 1 - Registrar vehículos 
 2 - Mostrar información del vehículo
 3 - Actualizar kilometraje
@@ -51,6 +52,7 @@ mensaje = """
 >>>: """
 
 class Vehiculo:
+
     contador = 0
 
     def __init__(self,patente,marca,anio,km):
@@ -58,22 +60,24 @@ class Vehiculo:
         self.marca = marca
         self.anio = anio
         self.km = km
+        Vehiculo.contador +=1
 
     def info_vehiculo(self):
-        print(f'La marca del vehiculo es {self.marca}, la patente {self.patente} su año {self.anio} y el kilometraje {self.km}')
+            print(f'La patente es {self.patente} la marca {self.marca} del año {self.anio} y el kilometraje del auto es {self.km}')
     
     def km_actual(self,km_recorrido):
         self.km += km_recorrido
-        print(f'El nuevo kilometraje una vez recorrido {km_recorrido} km es de {self.km} km')
+        print(f'\nEl nuevo kilometraje una vez recorrido {km_recorrido} km es de {self.km} km')
     
     @classmethod
     def contador_auto(cls):
-        cls.contador = len()
-
+        return cls.contador
+    
 lista_vehiculos = []
+
 # El menu del ecommerce
 while True:
-    menu = int(input(f'\nBienvenido/a a tu Ecommerce\n\nMenu\n\n1) Ver catálogo de productos\n2) Buscar producto por nombre o categoría\n3) Agregar producto al carrito\n4) Ver carrito y total\n5) Vaciar carrito\n0) Salir\n\nIngrese una opcion: '))
+    menu = int(input(f'\nBienvenido/a a tu Automotora\n\nMenu\n\n1) Registar vehiculo\n2) Mostrar informacion del vehiculo\n3) Actualizar kilometraje\n4) Contar cuantos vehiculos existen\n5) Salir\n\nIngrese una opcion: '))
     if menu == 1:
         patente = input('Ingrese la patente del vehiculo: ')
         marca = input('Ingresa la marca del vehiculo: ')
@@ -81,13 +85,31 @@ while True:
         km = int(input('Ingresa el kilometraje del vehiculo: '))
         auto = Vehiculo(patente,marca,anio,km)
         lista_vehiculos.append(auto)
-        print(lista_vehiculos)
+
+        
     elif menu ==2:
-        Vehiculo.info_vehiculo()
+        for i in lista_vehiculos:
+            i.info_vehiculo()
+
     elif menu ==3:
-        agregar_producto(int(input('Ingrese el id del producto: ')), int(input('Ingrese la cantidad del producto: ')))
+        actualizar_km_patente = input('Ingrese la patente del vehiculo a actualizar el kilometraje: ')
+        km_recorrido = int(input('Cuantos kilometros recorrio con el auto recientemente: '))
+
+        if km_recorrido < 0:
+            print('El kilometraje debe ser mayor a 0')
+        else:
+            encontrado = False
+            for i in lista_vehiculos:
+                if i.patente == actualizar_km_patente:
+                    i.km_actual(km_recorrido)
+                    encontrado = True
+                    break
+            if not encontrado:
+                print('No existe vehiculo con esa patente')
+
     elif menu ==4:
-        carrito_total()
+        print('Cantidad de autos registrados: ',Vehiculo.contador_auto())
+
     elif menu ==5:
         print('\nGracias por tu visita, te esperamos pronto')
         break
@@ -115,13 +137,86 @@ El sistema debe permitir:
 - Cancelar reservas
 - Mostrar información
 - Contar consultas activas
-
-
 """
 
 # 👉 AQUÍ COMIENZA TU SOLUCIÓN
-# class Consulta:
-#     pass
+class Consulta:
+
+    def __init__(self,hora_atencion,nombre_paciente,nombre_profesional):
+        self.hora_atencion = hora_atencion
+        self.nombre_paciente = nombre_paciente
+        self.nombre_profesional = nombre_profesional
+        self.estado = 'reservada'
+        self.agendado = []
+
+    def crear_reservas (self,lista_reserva):
+        self.agendado.append(lista_reserva)
+    
+    def cancelar_reservas (self):
+        self.estado = 'cancelada'
+
+    def mostrar_informacion(self):
+        print(f'El nombre del paciente {self.nombre_paciente.nombre_paciente} agendo la hora a las {self.hora_atencion} con {self.nombre_profesional.nombre_profesional} y el estado de la reserva esta {self.estado}')
+
+    def contar_consultas_activas(self,lista):
+        contador = 0
+        for i in lista:
+            if i.estado == 'reservada':
+                contador +=1
+        return contador
+
+
+lista_reservas = []
+
+class Paciente:
+
+    def __init__(self,nombre_paciente):
+        self.nombre_paciente = nombre_paciente
+
+class Profesional:
+    
+    def __init__(self,nombre_profesional):
+        self.nombre_profesional = nombre_profesional
+
+while True:
+    menu = int(input(f'\nBienvenido/a al Servicio de salud\n\nMenu\n\n1) Crear reservas\n2) Cancelar reservas\n3) Mostrar informacion\n4) Contar consultas activas\n5) Salir\n\nIngrese una opcion: '))
+
+    if menu == 1:
+        paciente = input('Ingrese su nombre: ')
+        profesional = input('Ingrese el nombre del profesional: ')
+        hora_atencion = input("Ingrese la hora de atención (Hora:Minuto): ")
+        p1 = Paciente(paciente)
+        pro1= Profesional(profesional)
+        con1= Consulta(hora_atencion,p1,pro1)
+        lista_reservas.append(con1)
+    
+    elif menu == 2:
+        paciente_cancelar = input('Ingrese su nombre: ')
+        encontrado = False
+        for i in lista_reservas:
+            if i.nombre_paciente.nombre_paciente == paciente_cancelar:
+                i.cancelar_reservas()
+                encontrado = True
+                break
+        if not encontrado:
+            print('Paciente el cual quiere cancelar la hora no tiene hora reservada')    
+
+    elif menu == 3:
+        for i in lista_reservas:
+            i.mostrar_informacion()
+
+    elif menu == 4:
+        if len(lista_reservas) == 0:
+            print('\nDebe crear reservas antes de consultar las activas')
+        else:
+            print(con1.contar_consultas_activas(lista_reservas))
+
+    elif menu ==5:
+        print('\nGracias por tu visita, te esperamos pronto')
+        break
+    else:
+        print('\nOpcion no valida, selecciona nuevamente')
+
 
 
 # =========================================================
